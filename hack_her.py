54836706095660a5c2e45919c241186bdb5bbe85
M-Janil <janiljugal@gmail.com>
2026-02-27 22:31:46 +0530

    Add basic Streamlit app functionality

--- hack_her.py
+++ hack_her.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.title("Hack Her App")
+
+st.write("This is my first Streamlit app 🎉")
+
+name = st.text_input("Enter your name")
+
+if st.button("Submit"):
+    st.success(f"Hello {name}!")
